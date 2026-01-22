e74d1f5f9885c9ca9116039507cb658481330934
张予星 <zhangyuxing@zhangyuxingdeMacBook-Pro-9.local>
2026-01-22 09:34:28 -0500

    Added red button with CSS and JS

---
+++ script.js
@@ -0,0 +1,8 @@
+// 1. Find the button in the HTML
+const btn = document.getElementById('myButton');
+
+// 2. Add an "event listener" to wait for the click
+btn.addEventListener('click', () => {
+    // 3. Print the message to the console
+    console.log("Hello world");
+});
